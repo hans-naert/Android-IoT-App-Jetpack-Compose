fb9bd22e03c465e4e9df66eca03a8438f712e012
Hans Naert <hans.naert@vives.be>
2023-05-14 11:23:16 +0200

    start with tempate empty activity with jetpack compose

---
+++ app/src/main/java/com/example/android_iot_app_deel_2/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.android_iot_app_deel_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_iot_app_deel_2.ui.theme.Android_IoT_App_Deel_2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Android_IoT_App_Deel_2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_IoT_App_Deel_2Theme {
+        Greeting("Android")
+    }
+}
