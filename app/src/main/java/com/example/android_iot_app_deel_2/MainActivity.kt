812d9c5fc9af5e7e2f25e0561254cd2bfd8090ef
Hans Naert <hans.naert@vives.be>
2023-05-14 17:07:17 +0200

    add picture of lamp and color

--- app/src/main/java/com/example/android_iot_app_deel_2/MainActivity.kt
+++ app/src/main/java/com/example/android_iot_app_deel_2/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.android_iot_app_deel_2
 
 import android.os.Bundle
+import android.os.Debug
+import android.util.Log
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_iot_app_deel_2.ui.theme.Android_IoT_App_Deel_2Theme
+import java.io.Console
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +32,13 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    //color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Lamp(true, { Log.d("Change lamp 1",if(it) "on" else "off") })
+                        Lamp()
+                        Lamp()
+                    }
                 }
             }
         }
@@ -30,17 +46,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Lamp(on: Boolean = false, toggle: (Boolean) -> Unit = {}) {
+    Card {
+        Image(painter= painterResource(id = if (on) R.drawable.lamp else R.drawable.lampoff),
+            contentDescription = "Lamp",
+            modifier=Modifier.background(Color.Blue)
+        )
+        Switch(checked = false, onCheckedChange = toggle )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LampPreview() {
+    Android_IoT_App_Deel_2Theme {
+        Surface(
+            //modifier = Modifier.fillMaxSize(),
+            //color = MaterialTheme.colorScheme.background
+        ) {
+           Lamp(true)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     Android_IoT_App_Deel_2Theme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column {
+                Lamp()
+                Lamp()
+                Lamp()
+            }
+        }
     }
 }
